43be8d102d9d9aea5fe4a9b893658b6cf5f07e9e
renga2k03 <119321014+renga2k03@users.noreply.github.com>
2025-12-02 20:16:39 +0530

    Update even-fibonacci-numbers.py

--- Q2/even-fibonacci-numbers.py
+++ Q2/even-fibonacci-numbers.py
@@ -1,1 +1,13 @@
-
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    sum = 2
+    a = 1
+    b = 2
+    while a + b < n:
+        c = a + b
+        if c % 2 == 0:
+            sum += c
+        a = b
+        b = c
+    print(sum)
