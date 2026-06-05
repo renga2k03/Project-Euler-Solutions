3f11df6dc8ef8715bac9338ba33d275132ff9dc5
renga2k03 <119321014+renga2k03@users.noreply.github.com>
2026-06-05 14:18:49 +0530

    Update largest-palindrome-product.py

--- Q4/largest-palindrome-product.py
+++ Q4/largest-palindrome-product.py
@@ -1,1 +1,19 @@
-
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    npalin = n // 1000
+    for i in range(npalin, 100, -1):
+        no = i * 1000 + int(''.join(list(str(i))[::-1]))
+        if no >= n:
+            continue
+        f = 0
+        nsqr = int(math.sqrt(no))
+        for i in range(100, nsqr+1):
+            if no % i == 0:
+                otherno = no // i
+                if len(str(otherno)) == 3:
+                    print(no)
+                    f = 1
+                    break
+        if f == 1:
+            break
