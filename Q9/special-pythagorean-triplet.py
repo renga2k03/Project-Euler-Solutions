f91f5b36d7f0d05b5005feca7caa32e961929c9c
renga2k03 <119321014+renga2k03@users.noreply.github.com>
2026-06-11 15:03:35 +0530

    Update special-pythagorean-triplet.py

--- Q9/special-pythagorean-triplet.py
+++ Q9/special-pythagorean-triplet.py
@@ -5,9 +5,10 @@ for a0 in range(t):
     maxprod = -1
     for c in range(n // 3, n // 2 + 1):
         prod = ((n * n - 2 * n * c) * c) // 2
-        if c * c - n * n + 2 * n * c >= 0:
-            a = ((n - c) + math.sqrt(c * c - n * n + 2 * n * c)) // 2
-            b = ((n - c) - math.sqrt(c * c - n * n + 2 * n * c)) // 2
+        d = c * c - n * n + 2 * n * c
+        if d >= 0:
+            a = ((n - c) - math.sqrt(d)) // 2
+            b = ((n - c) + math.sqrt(d)) // 2
             if maxprod < prod and a > 0 and b > 0 and a * a + b * b == c * c:
                 maxprod = prod
     print(maxprod)
